0ee69fe4186b23ce9bbc55b8437ca71cf1d8a40a
Rodika <raryku@yahoo.com>
2023-07-25 08:59:19 -0700

    function add

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ search.addEventListener('keyup', (e) => {
     const word = e.target.value.toLowerCase();
     myBooks.forEach(item => {
         item.querySelector('h3').textContent.toLowerCase().includes(word)
+        || item.querySelector('p').textContent.toLowerCase().includes(word)
         ? (item.style.display = "block")
         : (item.style.display = "none")
     })
